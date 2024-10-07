b675171be327859a583456858672001f1d0951cd
Temitayo <temfoden@gmail.com>
2024-10-07 23:45:47 +0100

    update script to comply with ESLINT standard

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,9 @@
 const getListStudents = () => {
-  let list = [
+  return [
     { id: 1, firstName: "Guillaume", location: "San Francisco" },
     { id: 2, firstName: "James", location: "Columbia" },
     { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
-  return list;
 };
 
 export default getListStudents;
