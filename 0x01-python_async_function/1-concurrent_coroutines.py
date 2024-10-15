75cabe94e76be665716356cb697f860c9a581680
Temitayo <afolabi.t@klakpad.com>
2024-10-15 01:26:14 +0100

    add function that imports another function as async await

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
 import asyncio
 from typing import List
 
-async def wait_n (n, max_delay: int)-> List[float]:
-    """Spawn wait_random n times, and return the list of all delays in ascending order"""
+
+# Dynamically import wait_random using __import__()
+# for the module with the number
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times, and return the list of all delays
+    in ascending order
+    """
     delays = []
     # Create asyncio tasks for each wait_randon coroutine
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
