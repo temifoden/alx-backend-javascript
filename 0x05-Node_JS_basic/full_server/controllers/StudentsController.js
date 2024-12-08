89ca32bbaabe7c5ee6e2abb1b6e34d0d72a42ccb
Temitayo <linuxmathew1245@gmail.com>
2024-12-08 20:05:24 +0100

    update: scripts to comply with semistandard linter

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,10 +1,10 @@
-const readDatabase = require("../utils");
+const readDatabase = require('../utils');
 
 module.exports = class StudentsController {
   static getAllStudents(request, response) {
     readDatabase(process.argv[2])
       .then((data) => {
-        let printData = "This is the list of our students";
+        let printData = 'This is the list of our students';
         for (const field in data) {
           if (Object.hasOwnProperty.call(data, field)) {
             const element = data[field];
@@ -13,22 +13,17 @@ module.exports = class StudentsController {
         }
         response.send(printData);
       })
-      .catch((err) => {
-        response.send(err.message);
-      });
+      .catch((err) => { response.send(err.message); });
   }
 
   static getAllStudentsByMajor(request, response) {
-    if (!["SWE", "CS"].includes(request.params.major))
-      response.status(500).send("Major parameter must be CS or SWE");
+    if (!['SWE', 'CS'].includes(request.params.major)) response.status(500).send('Major parameter must be CS or SWE');
     readDatabase(process.argv[2])
       .then((data) => {
         const printData = data[request.params.major].students;
         if (printData) response.send(printData);
-        response.status(500).send("Major parameter must be CS or SWE");
+        response.status(500).send('Major parameter must be CS or SWE');
       })
-      .catch((err) => {
-        response.send(err.message);
-      });
+      .catch((err) => { response.send(err.message); });
   }
 };
